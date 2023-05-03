246a10724c7c6bf3b11c50a58b2afc66af856e81
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2023-05-04 03:33:34 +0530

    Initial Commits

--- src/Componenents/Hero/Hero_section.jsx
+++ src/Componenents/Hero/Hero_section.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Hero_section.css'
+import {BsPlayCircle} from "react-icons/bs";
 
 const Hero_section = () => {
   return (
-    <div>Hero_section</div>
+    <section id="herosection">
+      <h4>AWESOME DESIGN</h4>
+      <h1>We love make things amazing and simple</h1>
+      <p>Maecenas class semper class semper sollicitudin lectus lorem iaculis imperdiet aliquam vehicula tempor auctor curabitur pede aenean ornare.</p>
+      <BsPlayCircle size="60px" color='#1bbc9d'/>
+    </section>
   )
 }
 
