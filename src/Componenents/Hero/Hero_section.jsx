d30926f4fbaa26ad0b2c58737e9fd8846e4035f5
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2023-05-27 17:35:00 +0530

    Initial Commits

--- src/Componenents/Hero/Hero_section.jsx
+++ src/Componenents/Hero/Hero_section.jsx
@@ -8,7 +8,7 @@ const Hero_section = () => {
       <h4>AWESOME DESIGN</h4>
       <h1>We love make things amazing and simple</h1>
       <p>Maecenas class semper class semper sollicitudin lectus lorem iaculis imperdiet aliquam vehicula tempor auctor curabitur pede aenean ornare.</p>
-      <BsPlayCircle size="60px" color='#1bbc9d'/>
+      <a href="https://www.youtube.com/watch?v=fzWzPXEhPvA&ab_channel=AEtemplates" target="_blank" rel="noreferrer"><BsPlayCircle size="60px" color='#1bbc9d'/></a>
     </section>
   )
 }
